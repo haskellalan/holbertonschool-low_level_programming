e13c448e71ce460323bca9e52565de1701515a4f
Alan Avila <3368@holbertonschool.com>
2021-06-17 18:02:58 -0300

    print size of var types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 printf("Size of a char: %ld(s) \n", sizeof(char));
 printf("Size of an int: %ld(s) \n", sizeof(int));
 printf("Size of a long int: %ld(s) \n", sizeof(long int));
-printf("Size of a long long int: %ld(s)\n", sizeof(long long int);
+ printf("Size of a long long int: %ld(s)\n", sizeof(long long int));
 printf("Size of a float: %ld(s) \n\n", sizeof(float));
 return (0);
 }
