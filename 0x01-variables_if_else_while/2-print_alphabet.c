b9254f8998e1238afc9e5d5cbb6d88f6da090b28
Alan Avila <3368@holbertonschool.com>
2021-06-18 15:41:06 -0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - print alphabet
+ *Return: end function
+ */
+
+int main(void)
+{
+char az = 'a';
+
+for (az = 'a'; az <= 'z'; az++)
+putchar(az);
+
+return (0);
+}
