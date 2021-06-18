4e37c4d6b1e37a19de1c2b5aa2b6dc5216d6e72c
Alan Avila <3368@holbertonschool.com>
2021-06-18 20:38:57 -0300

    print numberz from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - start function print 0 to 9
+*Return: end function
+*/
+
+int main(void)
+{
+int n0 = '0';
+int n = '\n';
+for (n0 = '0'; n0 <= '9' ; n0++)
+putchar(n0);
+putchar(n);
+return (0);
+}
