a245b151d32e94a97d3e0a960e446253362bacc6
Alan Avila <3368@holbertonschool.com>
2021-06-18 21:24:08 -0300

    print combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 int n0 = '0';
-int n = '\n';
-for (n0 = '0'; n0 <= '9' ; n0++)
+
+for (n0 = '0'; n0 <= '9'; n0++)
 {
 putchar(n0);
-if (n0 != 9)
+if (n0 != '9')
 {
 putchar(',');
 putchar(' ');
 }
 }
-putchar(n);
+putchar('\n');
 return (0);
 }
