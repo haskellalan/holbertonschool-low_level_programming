03487fafaff0c80b6660341f04e602c9b194eb99
Alan Avila <3368@holbertonschool.com>
2021-06-22 22:06:46 -0300

    print alphabetx10 function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 char alphabet;
 int _x10;
 
-for (_x10 = 0; _x10 <= 10; _x10++)
+for (_x10 = 0; _x10 < 10; _x10++)
 {
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	_putchar(alphabet);
