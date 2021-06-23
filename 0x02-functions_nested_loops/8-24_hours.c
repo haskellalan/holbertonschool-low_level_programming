83e503b589495da08b1b6a67f5eb754e46e5f880
Alan Avila <3368@holbertonschool.com>
2021-06-23 14:13:24 -0300

    print jack bauer hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ *jack_bauer - main function
+ *Return: return 0 to end function
+ *
+ */
+
+void jack_bauer(void)
+{
+	int minute;
+	int minute2;
+	int hour;
+	int hour2;
+
+	for (hour = 0; hour < 2; hour++)
+	{
+		for (hour2 = 0; hour2 <= 3; hour2++)
+		{
+			for (minute = 0; minute <= 5; minute++)
+			{
+				for (minute2 = 0; minute2 <= 9; minute2++)
+				{
+					_putchar(hour);
+					_putchar(hour2);
+					_putchar(':');
+					_putchar(minute);
+					_putchar(minute2);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
