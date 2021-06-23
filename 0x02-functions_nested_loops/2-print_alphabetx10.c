e817e9ce9b5e2bbdbe13ada0c9a3bea48ca64035
Alan Avila <3368@holbertonschool.com>
2021-06-22 21:51:19 -0300

    print alphabetx10 function

---
+++ 0x02-functions_nested_loops/2-print_alphabetx10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ * Return: 0 true
+ */
+
+void print_alphabet_x10(void);
+{
+char alphabet;
+char n = 'n\';
+int _x10;
+
+for (_x10 = 0; _x10 <= 10; _x10++)
+{
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+}
+_putchar(n);
+}
