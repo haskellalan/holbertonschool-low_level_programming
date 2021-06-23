05e60cd5fab126a25aee01e4eccf4e415fcfbbbf
Alan Avila <3368@holbertonschool.com>
2021-06-23 11:14:52 -0300

    test if c is alphanumeric

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,19 @@
 #include "holberton.h"
 
+/**
+ * _isalpha - function to test if c is alphanumeric
+ * Return: 0 if false 1 if true
+ * @c: value
+ */
+
 int _isalpha(int c)
 {
-	if (c <= 'a' && c >= 'z' && c <= 'A' && c >= 'Z')
-	{ 
-		return(1);
+	if ((c <= 'a' && c >= 'z') || (c <= 'A' && c >= 'Z'))
+	{
+		return (1);
 	}
 	else
-	{ 
-		return(0);
+	{
+		return (0);
 	}
 }
