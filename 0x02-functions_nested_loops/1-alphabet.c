93115d0f03171f7c853b086e31066fec6829698e
Alan Avila <3368@holbertonschool.com>
2021-06-22 21:00:15 -0300

    print alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - function to print alphabet
+ *Return: end function
+ */
+
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+	char n = '\n';
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+putchar(alphabet);
+putchar(n);
+return;
+}
