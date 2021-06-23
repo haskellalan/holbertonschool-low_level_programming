816ddfc2be7012b1a68fa76e3cfce0f95a730a3d
Alan Avila <3368@holbertonschool.com>
2021-06-23 12:59:17 -0300

    print last digit of n

---
+++ 0x02-functions_nested_loops/7-last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - print last digit of integer n
+ * Return: ends the function
+ * @n: value
+ */
+
+int print_last_digit(int n)
+{
+	int n = n % 10;
+
+	_putchar(n);
+}
